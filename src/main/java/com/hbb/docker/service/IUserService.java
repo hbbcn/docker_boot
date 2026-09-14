17df1444179a057fbdaa98e7d5b36abb86592774
hqbcn <hqbcn@outloo.com>
2026-09-14 23:00:06 +0800

    添加 subagent 测试注释

--- src/main/java/com/hbb/docker/service/IUserService.java
+++ src/main/java/com/hbb/docker/service/IUserService.java
@@ -14,4 +14,6 @@ import com.baomidou.mybatisplus.extension.service.IService;
 public interface IUserService extends IService<User> {
 
     // 测试subagent
+    // 测试subagent2
 }
+// subagent demo line
